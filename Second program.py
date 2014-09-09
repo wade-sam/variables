17b892b2fd9fefa60fb501564cbcd2693e47a2da
wade-sam <40404@longroad.ac.uk>
2014-09-09 11:06:20 +0100

    Second Pogram

---
+++ Second program.py
@@ -0,0 +1,8 @@
+#Sam Wade
+#09/09/2014
+
+#this is a vaiable that is storing th value entered by the user
+first_name = input("please enter your first name: ")
+print(first_name)
+#this ouputs the name in the format"Hi Sam!"
+print("Hi {0}!".format(first_name))
