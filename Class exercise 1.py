b460a2cbb3631374cefba3192c17bbc24ab744bc
wade-sam <40404@longroad.ac.uk>
2014-09-12 16:06:19 +0100

    Class Exercise 1

---
+++ Class exercise 1.py
@@ -0,0 +1,15 @@
+#sam Wade
+#12/9/14
+#Exercise: Revision exercise 1
+first_number = int(input(" Enter your first number)
+print = first_number                         
+second_number = int(input(" Enter your second number)
+print = second_number
+third_number = int(input(" Enter your third number)
+print = third_number
+fourth_number = int(input(" Enter your fourth number)
+print = fourth_number
+
+answer = first_number + second_number + third_number + fourth_number
+print = answer                          
+                          
