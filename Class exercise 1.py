0d05511c03f617230db5943eefc255585f816138
Sam Wade <40404@longroad.ac.uk>
2014-09-14 22:30:58 +0100

    Class exercise 1

--- Class exercise 1.py
+++ Class exercise 1.py
@@ -1,15 +1,17 @@
 #sam Wade
 #12/9/14
 #Exercise: Revision exercise 1
-first_number = int(input(" Enter your first number)
-print = first_number                         
-second_number = int(input(" Enter your second number)
-print = second_number
-third_number = int(input(" Enter your third number)
-print = third_number
-fourth_number = int(input(" Enter your fourth number)
-print = fourth_number
+first_number = int(input("Enter your first number"))
 
+second_number = int(input("Enter your second number"))
+
+third_number = int(input("Enter your third number"))
+
+fourth_number = int(input("Enter your fourth number"))
+print()
 answer = first_number + second_number + third_number + fourth_number
-print = answer                          
+
+print("The total of {0} + {1} + {2} + {3} + {4}.".format(first_number, second_number, third_number, fourth_number,answer))
+
                           
+
