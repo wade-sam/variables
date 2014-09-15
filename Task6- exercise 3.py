16df660b6e7cf5c9b3ebc0858c43c60470918491
Sam Wade <40404@longroad.ac.uk>
2014-09-15 21:01:28 +0100

    H/W 9/9/14:Task 6-exercise 3

---
+++ Task6- exercise 3.py
@@ -0,0 +1,8 @@
+#Sam Wade
+#Task6: exercise 3
+first_number = int(input("Please enter the width of the pool"))
+second_number = int(input("Please enter the length of the pool"))
+third_number = int(input("please enter the depth of the pool"))
+print()
+answer = first_number * second_number * third_number
+print("The volume of the pool is {3}cm3.".format(first_number, second_number, third_number, answer))
