c601e8429f07407764bd8e14087e1f3d0304f3dd
Sam Wade <40404@longroad.ac.uk>
2014-09-15 21:02:10 +0100

    H/W 9/9/14:Task 6-exercise 1

---
+++ Task 6- exercise 1.py
@@ -0,0 +1,8 @@
+#Sam Wade
+#Task 6 exercise 1
+first_number = int(input("Enter first number"))
+second_number = int(input("Enter second number"))
+third_number = int(input("Enter third number"))
+print()
+answer = first_number + second_number + third_number
+print("the total of {0} + {1} + {2} is {3}.".format(first_number, second_number, third_number, answer))
