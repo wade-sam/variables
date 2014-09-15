bddff9c036e2c7a92bbbb44bc92771de712818f5
Sam Wade <40404@longroad.ac.uk>
2014-09-15 21:02:29 +0100

    H/W 9/9/14:Task 6-exercise 2

---
+++ Question 6 exercise 2.py
@@ -0,0 +1,10 @@
+#Sam wade
+#Task 6 exercise 2
+
+first_number = int(input("please enter your first number"))
+second_number = int(input("please enter your second number"))
+print ()
+answer = first_number * second_number
+print("the total of {0} * {1} is {2}.".format(first_number,second_number,answer))
+
+
