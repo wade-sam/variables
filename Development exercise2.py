b16c7bea924add20a34a4f319d5d8d555159bc55
wade-sam <40404@longroad.ac.uk>
2014-09-16 11:12:22 +0100

    Development exercise2: 16/9/14

---
+++ Development exercise2.py
@@ -0,0 +1,9 @@
+#Sam Wade
+#Developments exercise 2
+#16/9/14
+
+first_number = int(input("Please enter the temperature in degrees Fahrenheit"))
+print()
+answer = (first_number - 32) * (5/9)
+print( "{0} degrees celcius.".format(answer))
+                         
