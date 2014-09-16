883875bc72e4d3aadddf93ce11327aa921a418e0
wade-sam <40404@longroad.ac.uk>
2014-09-16 11:11:34 +0100

    Development exercise1: 16/9/14

---
+++ Development exercise1.py
@@ -0,0 +1,10 @@
+#sam wade
+#development exercise number 1
+#dividing 2 numbers and finding the answer and remainder
+#16/9/14
+Number1 = int(input("Please enter first number"))
+Number2 = int(input("Please enter second number"))
+print()
+Total_number = Number1 / Number2
+remainder = Number1 % Number2
+print ("the answer is {0} with {1} remainder.".format(Total_number, remainder))
