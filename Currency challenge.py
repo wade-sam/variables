123946e42830ef85912f5c225e920775b803ae4d
wade-sam <40404@longroad.ac.uk>
2014-09-18 12:16:42 +0100

    Currency challenge 18/9/14

---
+++ Currency challenge.py
@@ -0,0 +1,28 @@
+#Sam Wade
+#Currency challenge
+#18/9/14
+
+whole_number = int(input("Please enter a whole number"))
+
+twenties = whole_number //20
+remainder_twenties = whole_number % 20
+
+tens = remainder_twenties // 10
+remainder_tens = remainder_twenties % 10
+
+fives = remainder_tens // 5
+remainder_fives = remainder_tens % 5
+
+twos = remainder_fives // 2
+remainder_twos = remainder_fives % 2
+
+ones = remainder_twos // 1
+remainder_ones = remainder_twos % 1
+
+print =(twenties)
+print =(tens)
+print =(fives)
+print =(twos)
+print =(ones)
+
+
